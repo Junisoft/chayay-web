447782407ad1bb151d79d9de051ee75934b5d9cb
aalvarez <aalvarez@webcreek.com>
2021-07-20 17:57:54 -0500

    changes in home component

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { CommonService } from 'src/app/services/common.service';
 
 @Component({
   selector: 'app-home',
@@ -12,11 +11,16 @@ export class HomeComponent implements OnInit {
   public phoneWA: string = '51925927848';
   public loQuieroLink: string = `https://wa.me/${this.phoneWA}?text=Hola, necesito información sobre internet para empresas`;
 
+  public carrouselImgs = [
+    {link: "", url:'assets/img/slide/sl-01-min.jpg'},
+    {link: "", url:'assets/img/slide/sl-02-min.jpg'},
+    {link: "", url:'assets/img/slide/sl-03-min.jpg'}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
-    // const state = this.router.url === '/inicio';
-    // this.commonService.sendData(state);
+
   }
 
 }
